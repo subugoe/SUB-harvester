8921d3f6af3100b125c0730dd2e235bcff543149
Sven-S. Porst <porst@sub.uni-goettingen.de>
2012-03-05 14:43:34 +0100

    setup for geo-leo-digitalisate

---
+++ configuration/geo-leo-digitalisate/settings.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Enthält die Einstellungen für den Harvester.
+ */
+
+
+// ***** MYSQL Verbindungsdaten *****
+include(dirname(__FILE__) . '/db_connect.php');
+
+
+
+// ***** Allgmein *****
+
+// Name des Dienstes; wird auf den Webseiten genutzt
+define("SERVICE_NAME", 		'GEO-LEO Digitalisate');
+
+// Adresse des Solr-Index
+define("SOLR",				'http://vlib.sub.uni-goettingen.de/solr');
+
+define("DATA_FOLDER",		'/var/www/htdocs/harvester/data/geo-leo-digitalisate');
+//define("DATA_FOLDER",		'C:/harvest_data');
+
+
+
+// ***** gemeinsam genutzte Einstellungen *****
+include(dirname(__FILE__) . '/../settings-common.php');
+
+
+?>
