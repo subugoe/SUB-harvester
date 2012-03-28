5250778d8e2d055b15376cd029fad8eb58dcb53d
Sven-S. Porst <porst@sub.uni-goettingen.de>
2012-03-28 12:09:38 +0200

    add test setup

---
+++ configuration/test/settings.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Enthält die Einstellungen für den Harvester.
+ */
+
+
+// ***** MYSQL Verbindungsdaten *****
+include(dirname(__FILE__) . '/db_connect.php');
+
+
+
+// ***** Allgmein *****
+
+// Name des Dienstes; wird auf den Webseiten genutzt
+define("SERVICE_NAME", 		'Test');
+
+// Adresse des Solr-Index
+define("SOLR",				'http://localhost:8080/solr/test');
+
+define("DATA_FOLDER",		'/var/www/htdocs/harvester/data/test');
+//define("DATA_FOLDER",		'C:/harvest_data');
+
+
+
+// ***** gemeinsam genutzte Einstellungen *****
+include(dirname(__FILE__) . '/../settings-common.php');
+
+
+?>
