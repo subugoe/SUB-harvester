df9bac0be1b10f9912ae58d6505e2dd08330568b
Sven-S. Porst <porst@sub.uni-goettingen.de>
2012-11-12 16:28:54 +0100

    add adw setup

---
+++ configuration/adw/settings.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Enthält die Einstellungen für den Harvester.
+ */
+
+
+// ***** MYSQL Verbindungsdaten *****
+include(dirname(__FILE__) . '/db_connect.php');
+
+
+
+// ***** Allgmein *****
+
+// Name des Dienstes; wird auf den Webseiten genutzt
+define("SERVICE_NAME", 		'Test');
+
+// Adresse des Solr-Index
+define("SOLR",				'http://localhost:8080/solr/adw');
+
+define("DATA_FOLDER",		'/var/www/htdocs/harvester/data/adw');
+
+define("METADATA_FORMAT",	'extpp');
+
+// File name of stylesheet in software/xsl to convert the downloaded OAI-XML to Solr XML.
+define("XSL_FILENAME",		'pica2solr.xsl');
+
+
+
+// ***** gemeinsam genutzte Einstellungen *****
+include(dirname(__FILE__) . '/../settings-common.php');
+
+
+?>
