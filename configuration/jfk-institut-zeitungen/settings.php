ef6a832c3a814a5e607f1c793bf9a59886b0c8a3
Sven-S. Porst <porst@sub.uni-goettingen.de>
2013-06-27 09:45:41 +0200

    FU Berlin / JFK Institut einrichten
    * Datenbank harvest10
    * Index jfk-institut-zeitungen
    * XSL zur Auswertung des gelieferten MARC Formats

---
+++ configuration/jfk-institut-zeitungen/settings.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Enthält die Einstellungen für den Harvester.
+ */
+
+
+// ***** MYSQL Verbindungsdaten *****
+include(dirname(__FILE__) . '/db_connect.php');
+
+
+
+// ***** Allgmein *****
+
+// Name des Dienstes; wird auf den Webseiten genutzt
+define("SERVICE_NAME", 		'Test');
+
+// Adresse des Solr-Index
+define("SOLR",				'http://solr-harvest.tc.sub.uni-goettingen.de/solr/jfk-institut-zeitungen');
+
+define("DATA_FOLDER",		'/var/www/htdocs/harvester/data/jfk-institut-zeitungen');
+
+define("METADATA_FORMAT",	'oai_dc');
+
+// File name of stylesheet in software/xsl to convert the downloaded OAI-XML to Solr XML.
+define("XSL_FILENAME",		'fu-marc2solr.xsl');
+
+
+
+// ***** gemeinsam genutzte Einstellungen *****
+include(dirname(__FILE__) . '/../settings-common.php');
+
+
+?>
